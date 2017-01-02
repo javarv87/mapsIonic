3731b4da71038afa67a6c7882e58e020de12ebce
Javier Ruiz Vázquez <java.rv87@gmail.com>
2017-01-02 16:43:57 -0600

    Se añaden estilos para el mapa y funciones para añadir marcador

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from 'ionic-native';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+    map: any;
 
   constructor(public navCtrl: NavController) {
-    
+
   }
 
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap(){
+    Geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log('Err: ', err);
+    });
+  }
+
+  addMarker(){
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h4>Information!</h4>";
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
 }
